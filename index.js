0689aaab2e66e0ea9a7f43a3b0a880b862790073
Mahmoud Khalifa <mahmoudkhalifa@hotmail.co.uk>
2018-04-30 21:31:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = "Hello, " + "Neil deGrasse Tyson"  + "!";
 
 
+
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = 'Let\'s talk about space.';
+=======
+var conversation = 'Let\'s talk about ${topic}.';
+>>>>>>> 6b4ce13e11e9880a9d2e998b4252c32af736957e
 
